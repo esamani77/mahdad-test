f57f377cb8606649817690b2d065f3ef5605c287
Erfan <esamani77@gmail.com>
2025-07-19 14:19:33 +0330

    feat: handle the layout

---
+++ components/Layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="absolute bottom-0 w-full min-h-9 bg-gray-200 flex justify-center items-center">
+      Footer
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/Layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="absolute top-0 w-full min-h-9 bg-gray-200 flex justify-center items-center">
+      Header
+    </div>
+  );
+};
+
+export default Header;
